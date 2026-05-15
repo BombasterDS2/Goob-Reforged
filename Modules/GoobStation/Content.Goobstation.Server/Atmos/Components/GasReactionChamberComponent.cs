89298cc2a5b2778d4417ad15737de5c3014c79ec
BombasterDS <bombasterds.github@mail.ru>
2026-05-15 13:57:31 +1000

    GasReactionChamber Component Part 1

---
+++ Modules/GoobStation/Content.Goobstation.Server/Atmos/Components/GasReactionChamberComponent.cs
@@ -0,0 +1,16 @@
+using Content.Server.Atmos.Reactions;
+
+namespace Content.Goobstation.Server.Atmos;
+
+/// <summary>
+/// Gas chamber that allows only specific gas reactions inside it
+/// </summary>
+[RegisterComponent]
+public sealed partial class GasReactionChamberComponent : Component
+{
+    /// <summary>
+    /// List of allowed reactions. Reactions are not allowed if empty.
+    /// </summary>
+    [DataField]
+    public HashSet<GasReactionPrototype> Reactions = new();
+}
